7d6269b4532bbf1ef6be13d12507c584de780ba4
David <edtg17@hotmail.com>
2023-04-04 01:03:42 -0500

    tipo_proveedor_respuestas

---
+++ resources/views/answers/loads/_form.blade.php
@@ -0,0 +1,24 @@
+{!! Form::model($answers, array('id' => 'answers_form','class' => 'form-horizontal', 'method' => $method)) !!}
+{!! Form::hidden('answers_id', $answers->id,['id'=>'answers_id']) !!}
+<div class="row">
+    <div class="col-md-6">
+        <div class="form-group">
+            {!! Form::label('answer','* Respuesta:', array('class' => 'control-label col-md-6')) !!}
+            <div class="col-md-12">
+                {!! Form::text('answer', $answers->answer, array('class' => 'form-control', 'autocomplete' =>
+                'off', 'placeholder' => 'ej. Ambiental', 'maxlength' => '64')) !!}
+            </div>
+        </div>
+    
+        <div class="form-group">
+        {!! Form::label('status','* Estado:', array('class' => 'control-label col-md-3')) !!}
+        <div class="col-md-12">
+            {!! Form::select('status', array( 'ACTIVE' => 'Activo', 'INACTIVE' => 'Inactivo'),$answers->status,array('class' => 'form-control') ) !!}
+        </div>
+        </div>
+</div>
+</div>
+
+
+
+{!! Form::close() !!}
---
+++ resources/views/typeProvider/index.blade.php
@@ -0,0 +1,35 @@
+@section('content')
+    @include('partials.admin_view',[
+    'title'=>'Administración de tipo de proveedor',
+    'icon'=>'<i class="flaticon-cogwheel-2"></i>',
+    'id_table'=>'typeProvider_table',
+    'action_buttons'=>[
+        [
+        'label'=>'Crear',
+        'icon'=>'<i class="la la-plus"></i>',
+        'handler_js'=>'newTypeProvider()',
+        'color'=>'btn-primary'
+        ],
+      ]
+    ])
+    @include('partials.modal',[
+    'title'=>'Crear tipo de proveedor',
+    'id'=>'modal',
+    'size'=>'modal-lg',
+    'action_buttons'=>[
+        [
+        'type'=>'submit',
+        'form'=>'typeProvider_form',
+        'id'=>'btn_save',
+        'label'=>'Guardar',
+        'color'=>'btn-primary'
+        ],
+     ]
+    ])
+    <input id="action_get_form" type="hidden" value="{{ route("getFormTypeProvider") }}"/>
+    <input id="action_save" type="hidden" value="{{ route("saveTypeProvider")}}"/>
+    <input id="action_list" type="hidden" value="{{ route("getListDataTypeProvider") }}"/>
+@endsection
+@section('additional-scripts')
+    <script src="{{asset("js/app/typeProvider/index.js")}}"></script>
+@endsection
