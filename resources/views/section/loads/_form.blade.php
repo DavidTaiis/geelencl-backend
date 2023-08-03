84384f521d9aeba73517926159b168d0908874f0
Lady Nazate <lady360@hotmail.com>
2023-08-03 15:26:43 -0500

    fix conflicts

--- resources/views/section/loads/_form.blade.php
+++ resources/views/section/loads/_form.blade.php
@@ -35,7 +35,7 @@
             {!! Form::label('name','* Total de puntos:', array('class' => 'control-label col-md-6')) !!}
             <div class="col-md-12">
                 {!! Form::number('totalPoints', $section->total_points, array('class' => 'form-control', 'autocomplete' =>
-                'off', 'placeholder' => 'ej. 60', 'maxlength' => '64' ,'min'=>'1')) !!}
+                'off', 'placeholder' => 'ej. 60', 'maxlength' => '64' ,'min'=>'0')) !!}
             </div>
         </div>
         <div class="form-group">
